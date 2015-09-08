facbf18c014f6af16ea68e8de53d271382a24ab5
Jonas <jtb110@hotmail.com>
2015-09-08 12:44:01 +0200

    added server properties and added port+ip to server class

--- src/ca1/ChatServer.java
+++ src/ca1/ChatServer.java
@@ -5,17 +5,25 @@
  */
 package ca1;
 
+import java.util.Properties;
+import utils.Utils;
+
 /**
  *
  * @author Jonas
  */
 public class ChatServer {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final Properties properties = Utils.initProperties("server.properties");
+
+    public void runServer() {
+
+        int port = Integer.parseInt(properties.getProperty("port"));
+        String ip = properties.getProperty("serverIp");
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
